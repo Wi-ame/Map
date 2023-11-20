698f1a995d2c6812d95bde7f0e7122bb9d908593
WIAME <wiwimess92@gmail.com>
2023-11-20 23:06:37 +0100

    Initial commit

---
+++ app/src/main/java/com/cscorner/map/MainActivity.java
@@ -0,0 +1,20 @@
+package com.cscorner.map;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.FirebaseApp;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FirebaseApp.initializeApp(this);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
